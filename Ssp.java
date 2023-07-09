9fd0a222d943369ef147ff252e44ec564dd87169
Ganesh Anarse <ganeshanarse99@gmail.com>
2023-07-09 11:59:20 +0530

    Resolved errors

--- Ssp.java
+++ Ssp.java
@@ -1,4 +1,4 @@
-public class Kingdom{
+class Kingdom{
 	private String name;
 	
 	public String getName(){
@@ -11,12 +11,12 @@ public class Kingdom{
 	
 }
 
-class Ssp{
+public class Ssp{
 	
 	public static void main(String []args){
 		Kingdom obj = new Kingdom();
 		obj.setName("Saurabh S Patil");
-		System.out.println("Print the name "+getName());
+		System.out.println("Print the name "+obj.getName());
 		
 	}
 }
